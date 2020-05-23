0ff56822ff2675be42b64db4d556e4c66cb59771
Nitish Singh <nsingh07@syr.edu>
2020-05-23 19:27:16 -0400

    Converted Vets to JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,6 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
     private String description;
 
     public String getDescription() {
