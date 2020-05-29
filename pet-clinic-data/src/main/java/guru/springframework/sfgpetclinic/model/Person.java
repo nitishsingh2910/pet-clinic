f908158fad5356061f59a62d79533a249078f8b1
Nitish Singh <nsingh07@syr.edu>
2020-05-29 13:47:46 -0400

    Added project Lombok to the project. Closes #51

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
     @Column(name="first_name")      // redundant
@@ -10,6 +17,12 @@ public class Person extends BaseEntity {
 
     @Column(name="last_name")
     private String lastName;
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public String getFirstName() {
         return firstName;
